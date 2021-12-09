ee399d2f060e5f8b6b972fa2169759b96becf8c0
kurtreyn <kurtreyn@yahoo.com>
2021-12-09 01:21:20 -0500

    added api for products

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,7 +1,9 @@
 'use strict';
+
 const shoppingCart = document.querySelector('.shopping-cart');
-const products = document.querySelectorAll('[data-product]');
+// const products = document.querySelectorAll('[data-product]');
 const cartBtns = document.querySelectorAll('[data-button]');
+// https://github.com/keikaavousi/fake-store-api
 
 function clickTest() {
   console.log(`button was clicked`);
@@ -10,3 +12,23 @@ function clickTest() {
 for (const cartBtn of cartBtns) {
   cartBtn.addEventListener('click', clickTest);
 }
+
+function loadProducts() {
+  fetch(`https://fakestoreapi.com/products`, {
+    method: 'GET',
+  })
+    .then((response) => response.json())
+    .then((data) => {
+      // console.log(data);
+
+      for (let i = 0; i < data.length; i++) {
+        // console.log(data[i]);
+        const description = data[i].category;
+        console.log(description);
+      }
+    })
+    .catch((err) => {
+      console.error(err);
+    });
+}
+loadProducts();
