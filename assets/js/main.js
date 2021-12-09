c0e0879b11a0b5a423bf834a719cca0f95924f3d
kurtreyn <kurtreyn@yahoo.com>
2021-12-08 22:05:06 -0500

    added shopping cart and started js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,12 @@
 'use strict';
+const shoppingCart = document.querySelector('.shopping-cart');
+const products = document.querySelectorAll('[data-product]');
+const cartBtns = document.querySelectorAll('[data-button]');
+
+function clickTest() {
+  console.log(`button was clicked`);
+}
+
+for (const cartBtn of cartBtns) {
+  cartBtn.addEventListener('click', clickTest);
+}
