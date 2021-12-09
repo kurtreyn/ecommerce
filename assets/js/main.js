9b671045a91678f68c05473787617c381bfe2af9
KurtReyn <kurtreyn@yahoo.com>
2021-12-09 13:03:59 -0500

    continued working on api

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,9 +1,59 @@
 'use strict';
 
-const shoppingCart = document.querySelector('.shopping-cart');
-// const products = document.querySelectorAll('[data-product]');
-const cartBtns = document.querySelectorAll('[data-button]');
 // https://github.com/keikaavousi/fake-store-api
+const shoppingCart = document.querySelector('.shopping-cart');
+const products = document.querySelectorAll('[data-product]');
+const cartBtns = document.querySelectorAll('[data-button]');
+const title = document.querySelectorAll('[data-title]');
+
+const url = `https://fakestoreapi.com/products`;
+const mensClothing = [];
+const womensClothing = [];
+
+async function getProducts() {
+  const response = await fetch(url);
+  const data = await response.json();
+  // console.log(data);
+
+  data.forEach((dat) => {
+    // console.log(`${dat.category}`);
+
+    Object.entries(dat).forEach(([key, value]) => {
+      // console.log(`${key} ${value}`);
+      if (dat.category === `men's clothing`) {
+        mensClothing.push(dat);
+      } else if (dat.category === `women's clothing`) {
+        womensClothing.push(dat);
+      }
+    });
+  });
+
+  for (let i = 0; i < mensClothing.length; i++) {
+    console.log(mensClothing[i]);
+
+    // let title = mensClothing[i].title;
+    // console.log(title);
+  }
+}
+
+getProducts();
+
+// function getValue() {
+//   for (let p = 0; p < products.length; p++) {
+//     let productVal = products[p].getAttribute('data-product');
+//     // console.log(productVal);
+//     if (products[p].value === 'jacket') {
+//       title.innerText = '';
+//     }
+//   }
+// }
+
+// getValue();
+
+// id: 3, id: 16, id: 18, id:19
+
+// console.log(mensClothing);
+// console.log(womensClothing);
 
 function clickTest() {
   console.log(`button was clicked`);
@@ -12,23 +62,3 @@ function clickTest() {
 for (const cartBtn of cartBtns) {
   cartBtn.addEventListener('click', clickTest);
 }
-
-function loadProducts() {
-  fetch(`https://fakestoreapi.com/products`, {
-    method: 'GET',
-  })
-    .then((response) => response.json())
-    .then((data) => {
-      // console.log(data);
-
-      for (let i = 0; i < data.length; i++) {
-        // console.log(data[i]);
-        const description = data[i].category;
-        console.log(description);
-      }
-    })
-    .catch((err) => {
-      console.error(err);
-    });
-}
-loadProducts();
