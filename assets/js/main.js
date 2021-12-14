c1517c4fdecfa47c0420472a41530566a941b5fa
kurtreyn <kurtreyn@yahoo.com>
2021-12-13 22:53:50 -0500

    re-worked js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,72 +1,91 @@
 'use strict';
 
+// https://youtu.be/023Psne_-_4?t=6376
+
 // https://github.com/keikaavousi/fake-store-api
 const shoppingCart = document.querySelector('.shopping-cart');
 const products = document.querySelectorAll('[data-product]');
+const productsDOM = document.querySelector('.product-container');
 const cartBtns = document.querySelectorAll('[data-button]');
 const title = document.querySelectorAll('[data-title]');
 const card = document.querySelectorAll('.card');
 let thisTitle = '';
 
-const url = `https://fakestoreapi.com/products`;
+const url = '../products.json';
 const mensClothing = [];
 const womensClothing = [];
 
-async function getProducts() {
-  const response = await fetch(url);
-  const data = await response.json();
-  // console.log(data);
-
-  for (let x = 0; x < data.length; x++) {
-    // console.log(data[x]);
-    if (data[x].category === `men's clothing`) {
-      mensClothing.push(data[x]);
-    } else if (data[x].category === `women's clothing`) {
-      womensClothing.push(data[x]);
+// GET PRODUCTS
+class Products {
+  async getProducts() {
+    try {
+      let result = await fetch(url);
+      let data = await result.json();
+      let products = data.items;
+      products = products.map((item) => {
+        const { title, price, description } = item.fields;
+        const { id } = item.sys;
+        const image = item.fields.image.fields.file.url;
+        return { title, price, description, id, image };
+      });
+      return products;
+    } catch (error) {
+      console.log(error);
     }
   }
-  loadProducts();
 }
 
-// console.log(mensClothing);
-// console.log(womensClothing);
-
-getProducts();
-
-function loadProducts() {
-  // let thisProduct = this.getAttribute('data-product');
-  // thisTitle = this.getAttribute('data-title');
-
-  for (let y = 0; y < mensClothing.length; y++) {
-    // console.log(mensClothing[y].image);
-    let image = mensClothing[y].image;
-    let currentTitle = mensClothing[y].title;
-    if (currentTitle === mensClothing[y].title) {
-    }
+// DISPLAY PRODUCTS
+class UI {
+  displayProducts(products) {
+    // console.log(products);
+    let result = '';
+    products.forEach((product) => {
+      result += `
+      <div
+          class="
+            col-12 col-sm-12 col-md-3 col-lg-3 col-xl-3
+            text-center
+            products
+          "
+        >
+      <div class="card h-100">
+      <img
+        src=${product.image}
+        class="card-img-top"
+        alt="purse"
+      />
+      <div class="card-body d-flex flex-column" data-product="purse">
+        <h5 class="card-title" data-title="purse">${product.title}</h5>
+        <p class="card-text">
+          ${product.description}
+        </p>
+        <h6>$${product.price}</h6>
+        <button
+          type="button"
+          class="btn add-cart-btn mt-auto"
+          data-button=${product.id}
+        >
+          add to cart
+        </button>
+      </div>
+    </div>
+    </div>
+      `;
+    });
+    productsDOM.innerHTML = result;
   }
-
-  for (let t = 0; t < title.length; t++) {
-    let titleVal = title[t].getAttribute('data-title');
-    // console.log(titleVal);
-  }
 }
 
-// function getValue() {
-//   for (let i = 0; i < title.length; i++) {
-//     // console.log(title[i]);
-//     let val = title[i].getAttribute('data-title');
-//     // console.log(val);
-//   }
-// }
-
-// getValue();
-
-// id: 3, id: 16, id: 18, id:19
-
-function clickTest() {
-  console.log(`button was clicked`);
+// LOCAL STORAGE
+class Storage {
+  // -
 }
 
-for (const cartBtn of cartBtns) {
-  cartBtn.addEventListener('click', clickTest);
-}
+// EVENT LISTENER
+document.addEventListener('DOMContentLoaded', () => {
+  const ui = new UI();
+  const products = new Products();
+
+  products.getProducts().then((products) => ui.displayProducts(products));
+});
